dba8a0b3601a7a5edd156bbbd6d1fb35d4b90457
ATsoi418 <ashley.s.tsoi@gmail.com>
2021-01-31 23:00:47 -0800

    pre homepage restyle

--- src/Components/pages/dashboardComponents/dashboard.js
+++ src/Components/pages/dashboardComponents/dashboard.js
@@ -1,14 +1,10 @@
 import React, { Component } from 'react';
-import FlipClock from './flipClock.js';
 import Calendar from './calendar.js';
 
 class Dashboard extends Component {
   render() {
     return (
       <div className="container-fluid">
-          <div id="flipClock">
-            <FlipClock />
-          </div>
           <div id="calendar">
             <Calendar />
           </div>
