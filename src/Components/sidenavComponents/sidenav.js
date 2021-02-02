24492680b2a127aa9c548c8d250f7a597651a3a4
ATsoi418 <ashley.s.tsoi@gmail.com>
2021-02-01 18:01:03 -0800

    sidebar add dropdown

--- src/Components/sidenavComponents/sidenav.js
+++ src/Components/sidenavComponents/sidenav.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faNewspaper,faSolarPanel,faShapes } from '@fortawesome/free-solid-svg-icons'
+import { faNewspaper,faSolarPanel,faShapes,faLightbulb } from '@fortawesome/free-solid-svg-icons'
 
 class Sidenav extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      menu: false
+    };
+    this.toggleMenu = this.toggleMenu.bind(this);
+  }
+
+  toggleMenu(){
+    this.setState({ menu: !this.state.menu })
+  }
+
   render() {
+
+    const show = (this.state.menu) ? "show" : "" ;
+    
     return (
       <div className="sidenav">
         <div className="logo">
@@ -29,6 +45,23 @@ class Sidenav extends Component {
                 <span className="navIcon"><FontAwesomeIcon icon={faShapes} size="sm"/></span>
                 <span className="navText">Project Monitor</span>
               </Link>
+            </li>
+            <li>
+              <Link to="/" className="dropdown-toggle">
+                <span className="navIcon"><FontAwesomeIcon icon={faLightbulb} size="sm"/></span>
+                <span className="navText">Solvers</span>
+              </Link>
+              <button className="navbar-toggler" type="button" onClick={ this.toggleMenu }>
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className={"collapse navbar-collapse " + show}>
+                <ul className="navbar-nav">
+                  <li><Link to="/" className="">Home</Link></li>
+                  <li><Link to="/" className="">Features</Link></li>
+                  <li><Link to="/" className="">Pricing</Link></li>
+                  <li><Link to="/" className="">logout</Link></li>
+                </ul>
+              </div>
             </li>
           </ul>
         </nav>
